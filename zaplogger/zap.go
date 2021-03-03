c8051f09138ce68368df04a59931d41d8f2c8370
nevercase <1024769485@qq.com>
2021-03-03 12:15:05 +0800

    customized zaplogger time

--- zaplogger/zap.go
+++ zaplogger/zap.go
@@ -2,7 +2,9 @@ package zaplogger
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"log"
+	"time"
 )
 
 var logger *zap.Logger
@@ -10,7 +12,7 @@ var sugar *zap.SugaredLogger
 
 func init() {
 	var err error
-	logger, err = zap.NewProduction()
+	logger, err = NewProductionConfig().Build()
 	if err != nil {
 		log.Panic(err)
 	}
@@ -27,4 +29,52 @@ func Sugar() *zap.SugaredLogger {
 
 func Logger() *zap.Logger {
 	return logger
+}
+
+// NewProductionConfig is a reasonable production logging configuration.
+// Logging is enabled at InfoLevel and above.
+//
+// It uses a JSON encoder, writes to standard error, and enables sampling.
+// Stacktraces are automatically included on logs of ErrorLevel and above.
+func NewProductionConfig() zap.Config {
+	return zap.Config{
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Development: false,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding:         "json",
+		EncoderConfig:    NewProductionEncoderConfig(),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+}
+
+// NewProductionEncoderConfig returns an opinionated EncoderConfig for
+// production environments.
+func NewProductionEncoderConfig() zapcore.EncoderConfig {
+	return zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		FunctionKey:    zapcore.OmitKey,
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     EpochTimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+}
+
+// EpochTimeEncoder serializes a time.Time to a floating-point number of seconds
+// since the Unix epoch.
+func EpochTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	//nanos := t.UnixNano()
+	//sec := float64(nanos) / float64(time.Second)
+	//enc.AppendFloat64(sec)
+	enc.AppendString(t.Format("2006-01-02T15:04:05.000Z"))
 }
