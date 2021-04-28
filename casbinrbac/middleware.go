35c960e89e6eeefe56a23b7862e09d0710126af6
nevercase <1024769485@qq.com>
2021-04-28 16:24:09 +0800

    casbinrbac middle abort

--- casbinrbac/middleware.go
+++ casbinrbac/middleware.go
@@ -4,31 +4,32 @@ import (
 	"github.com/Shanghai-Lunara/pkg/jwttoken"
 	"github.com/Shanghai-Lunara/pkg/zaplogger"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Handler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.Request.Header.Get(jwttoken.TokenKey)
 		if token == "" {
-			c.Abort()
+			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		tokenClaims, err := jwttoken.Parse(token)
 		if err != nil {
 			zaplogger.Sugar().Error(err)
-			c.Abort()
+			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		ok, err := Enforce(tokenClaims.Username, c.Param("namespace"), c.Param("permission"), c.Param("action"))
 		if err != nil {
 			zaplogger.Sugar().Error(err)
-			c.Abort()
+			c.AbortWithStatus(http.StatusForbidden)
 			return
 		}
 		if ok {
 			c.Next()
 		} else {
-			c.Abort()
+			c.AbortWithStatus(http.StatusForbidden)
 		}
 	}
 }
